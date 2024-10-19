f4b193d45176b9349007c6555d1f4c841327f6d9
David Soldat <davidsoldat00@gmail.com>
2024-10-19 19:21:28 +0200

    adding modal

--- app/_components/ListingModal.js
+++ app/_components/ListingModal.js
@@ -1,7 +1,43 @@
+// 'use client';
+
+// import { Dialog, DialogContent, DialogOverlay } from '@/components/ui/dialog';
+// import { useRouter } from 'next/navigation';
+
+// export default function ListingModal({ children }) {
+//   const router = useRouter();
+//   const handleOpenChange = () => {
+//     router.back();
+//   };
+
+//   return (
+//     <Dialog open={true} onOpenChange={handleOpenChange}>
+//       <DialogOverlay>
+//         <DialogContent
+//           aria-describedby={undefined}
+//           className="h-screen !rounded-none bg-white p-0 md:max-w-6xl lg:mx-auto [&>button]:hidden"
+//         >
+//           {children}
+//         </DialogContent>
+//       </DialogOverlay>
+//     </Dialog>
+//   );
+// }
+
 'use client';
 
-import { Dialog, DialogContent, DialogOverlay } from '@/components/ui/dialog';
+import { Button } from '@/components/ui/button';
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogOverlay,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
 import { useRouter } from 'next/navigation';
+import { MdArrowBackIosNew, MdIosShare } from 'react-icons/md';
+import Logo from './Logo';
+import { VisuallyHidden } from '@radix-ui/react-visually-hidden';
 
 export default function ListingModal({ children }) {
   const router = useRouter();
@@ -14,8 +50,22 @@ export default function ListingModal({ children }) {
       <DialogOverlay>
         <DialogContent
           aria-describedby={undefined}
-          className="h-screen !rounded-none bg-white p-0 md:max-w-6xl lg:mx-auto [&>button]:hidden"
+          className="flex h-screen flex-col gap-0 overflow-y-scroll !rounded-none bg-white p-0 md:max-w-6xl lg:mx-auto [&>button]:hidden"
         >
+          <DialogTitle>
+            <VisuallyHidden>Property</VisuallyHidden>
+          </DialogTitle>
+          <div className="sticky top-0 z-10 flex h-fit w-full items-center justify-between bg-white p-4">
+            <DialogTrigger asChild>
+              <Button className="flex gap-3 text-sm font-normal leading-6 text-gray-600 hover:underline">
+                <MdArrowBackIosNew size={25} /> Back to search
+              </Button>
+            </DialogTrigger>
+            <Logo />
+            <Button className="flex gap-1 text-sm font-normal leading-6 text-gray-600 hover:underline">
+              <MdIosShare size={20} /> Share
+            </Button>
+          </div>
           {children}
         </DialogContent>
       </DialogOverlay>
--- app/_components/PropertyPage.js
+++ app/_components/PropertyPage.js
@@ -1,19 +1,103 @@
 import Image from 'next/image';
-import mainImage from '@/assets/images/prop1.jpg';
-import Link from 'next/link';
+import { convertNumber, formatPrice } from '../_lib/helpers';
+import { Button } from '@/components/ui/button';
 
 export default function PropertyPage({ property }) {
+  const firstImage = property.images[0];
   return (
-    <div className="w-full p-4">
-      <div className="w-full border-b-2">
-        <Link href="/buy">Back to search</Link>
+    <div className="flex w-full flex-col gap-5 px-5">
+      <div className="photo-grid grid h-[440px] grid-cols-4 grid-rows-2 gap-2 overflow-hidden rounded-lg">
+        <div className="relative col-span-2 row-span-2 h-full">
+          <Image
+            alt="Property image"
+            src={firstImage}
+            fill
+            priority
+            className="object-cover"
+          />
+        </div>
+        {property.images.slice(1).map((image, i) => (
+          <div className="relative h-full" key={i}>
+            <Image
+              alt="Property image"
+              src={image}
+              fill
+              priority
+              className="object-cover"
+            />
+          </div>
+        ))}
       </div>
-      <div className="photo-grid gap-2">
-        <Image alt="image" src={mainImage} className="main-image" />
-        <Image alt="image" className="second-image" src={mainImage} />
-        <Image alt="image" className="third-image" src={mainImage} />
-        <Image alt="image" className="fourth-image" src={mainImage} />
-        <Image alt="image" className="fifth-image" src={mainImage} />
+      <div className="flex w-full gap-5">
+        <div className="flex w-[70%] flex-col gap-5">
+          <div className="flex justify-between border-b border-gray-300 pb-5">
+            <div className="flex flex-col">
+              <span className="text-3xl font-bold text-gray-800">
+                {formatPrice(property.price)}
+              </span>
+              <div>
+                <h1 className="text-xl font-light leading-8">
+                  {property.address}, {property.city}
+                </h1>
+              </div>
+            </div>
+            <div className="flex items-start gap-4 text-xl font-normal leading-8 text-gray-800">
+              <div className="flex flex-col items-start justify-center">
+                <span className="text-3xl font-bold">
+                  {convertNumber(property.bedrooms)}
+                </span>
+                beds
+              </div>
+              <div className="flex flex-col items-start justify-center">
+                <span className="text-3xl font-bold">
+                  {convertNumber(property.bathrooms)}
+                </span>
+                baths
+              </div>
+              <div className="flex flex-col items-start justify-center">
+                <span className="text-3xl font-bold">{property.sqmeter}</span>
+                sq m
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col gap-5">
+            <h1 className="text-2xl font-bold text-gray-800">
+              More about this property
+            </h1>
+            <div>
+              <p>
+                Completely renovated light filled 1-bedroom condo in the highly
+                desirable Dearborn Park neighborhood of the South Loop. Living
+                room/dining room combination features wood flooring and corner
+                window. The open kitchen offers granite breakfast bar and
+                countertops, stainless steel appliances, under mount cabinet
+                lighting and tile backsplash. Very large bedroom with double
+                closets (one is huge walk-in closet). The bathroom has
+                kitchen-height granite vanity and shower/tub combo. Move right
+                in!!! Part-time door staff, onsite management, building
+                engineer, additional storage, receiving room and community room
+                and 24/7 neighborhood security. Parking for rent or purchase in
+                the attached garage. In turn-key condition, this super cool
+                space is in turn-key condition, in the BEST location and has a
+                very high walk score. Close to all public transportation, the
+                Lake, the Loop, Target, Jewel, Whole Foods, Marianos, Trader
+                Joes, Grant Park and easy access to all expressways!
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="flex h-fit w-1/3">
+          <div className="contactProperty sticky top-10 flex w-full flex-col items-center justify-center gap-5 p-5">
+            <button className="!important flex w-full flex-col items-center justify-center rounded-md border bg-blue-600 px-4 py-2 text-base font-bold text-white hover:bg-blue-700">
+              <span>Request a tour</span>
+              <p className="text-xs font-light">as early as Monday!</p>
+            </button>
+
+            <Button className="w-full border border-blue-600 px-4 py-2 text-base font-bold text-blue-600 hover:border-blue-700 hover:text-blue-700">
+              Message
+            </Button>
+          </div>
+        </div>
       </div>
     </div>
   );
